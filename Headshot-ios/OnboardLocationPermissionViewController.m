d3269e56dca3a49e8ded42f063db9f268e387f54
Jeff Ames <jeffreymames@gmail.com>
2014-06-24 17:49:55 -0400

    added content to location permission view

--- Headshot-ios/OnboardLocationPermissionViewController.m
+++ Headshot-ios/OnboardLocationPermissionViewController.m
@@ -34,6 +34,31 @@
     locationImageView.centerX = self.view.width/2.0;
     [self.scrollView addSubview:locationImageView];
     
+    UILabel *wellDoneLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, locationImageView.bottom + 37, self.view.width, 31)];
+    wellDoneLabel.text = @"Well Done!";
+    wellDoneLabel.font = [ThemeManager regularFontOfSize:26];
+    wellDoneLabel.textAlignment = NSTextAlignmentCenter;
+    wellDoneLabel.textColor = [[ThemeManager sharedTheme] greenColor];
+    [self.scrollView addSubview:wellDoneLabel];
+    
+    UILabel *allSetLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, wellDoneLabel.bottom, self.view.width, 45)];
+    allSetLabel.text = @"You are all set up. Just two last steps...";
+    allSetLabel.font = [ThemeManager regularFontOfSize:15];
+    allSetLabel.textAlignment = NSTextAlignmentCenter;
+    allSetLabel.textColor = [[ThemeManager sharedTheme] lightGrayTextColor];
+    [self.scrollView addSubview:allSetLabel];
+    
+    UILabel *allowTrackingLabel = [[UILabel alloc] init];
+    allowTrackingLabel.size = CGSizeMake(260, 70);
+    allowTrackingLabel.centerX = self.view.width/2.0;
+    allowTrackingLabel.y = allSetLabel.bottom + 30;
+    allowTrackingLabel.text = @"Allow location services to let your team know when you are at your office. We are going to track only your office location";
+    allowTrackingLabel.textAlignment = NSTextAlignmentCenter;
+    allowTrackingLabel.numberOfLines = 3;
+    allowTrackingLabel.font = [ThemeManager regularFontOfSize:15];
+    allowTrackingLabel.textColor = [[ThemeManager sharedTheme] lightGrayTextColor];
+    [self.scrollView addSubview:allowTrackingLabel];
+    
     self.permissionButton = [UIButton buttonWithType:UIButtonTypeCustom];
     self.permissionButton.size = CGSizeMake(self.view.width, 60);
     self.permissionButton.bottom = self.scrollView.contentSize.height;
