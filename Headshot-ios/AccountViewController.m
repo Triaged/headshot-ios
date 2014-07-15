3b7e005d66592a57f050ed0ec9839292f0b8af98
Jeff Ames <jeffreymames@gmail.com>
2014-07-15 18:09:04 -0400

    prompt user to complete profile

--- Headshot-ios/AccountViewController.m
+++ Headshot-ios/AccountViewController.m
@@ -10,6 +10,7 @@
 #import "EmployeeInfo.h"
 #import "OfficeLocation.h"
 #import "OnboardNavigationController.h"
+#import "EditAccountViewController.h"
 #import "CredentialStore.h"
 #import "ContactDetailsDataSource.h"
 #import "SettingsViewController.h"
@@ -19,6 +20,8 @@
 @interface AccountViewController ()
 
 @property (nonatomic, strong) ContactDetailsDataSource *contactDetailsDataSource;
+@property (strong, nonatomic) UIView *completeProfileView;
+@property (assign, nonatomic) BOOL hasShownCompleteProfileView;
 
 @end
 
@@ -57,6 +60,23 @@
     
     [self loadViewFromData];
     
+    self.completeProfileView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.width, 48)];
+    self.completeProfileView.backgroundColor = [[ThemeManager sharedTheme] orangeColor];
+    UITapGestureRecognizer *completeProfileTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(completeProfileTapped:)];
+    [self.completeProfileView addGestureRecognizer:completeProfileTap];
+    UILabel *completeProfileLabel = [[UILabel alloc] initWithFrame:self.completeProfileView.bounds];
+    completeProfileLabel.text = @"Complete your profile";
+    completeProfileLabel.textAlignment = NSTextAlignmentCenter;
+    completeProfileLabel.font = [ThemeManager regularFontOfSize:16];
+    completeProfileLabel.textColor = [UIColor whiteColor];
+    [self.completeProfileView addSubview:completeProfileLabel];
+    UIButton *cancelCompleteProfileButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    cancelCompleteProfileButton.size = CGSizeMake(25, 25);
+    cancelCompleteProfileButton.x = 15;
+    cancelCompleteProfileButton.centerY = self.completeProfileView.height/2.0;
+    [cancelCompleteProfileButton setImage:[UIImage imageNamed:@"cancel_white"] forState:UIControlStateNormal];
+    [cancelCompleteProfileButton addTarget:self action:@selector(dismissCompleteProfileView) forControlEvents:UIControlEventTouchUpInside];
+    [self.completeProfileView addSubview:cancelCompleteProfileButton];
 }
 
 -(void) viewWillAppear:(BOOL)animated {
@@ -66,7 +86,44 @@
     [self loadViewFromData];
     // refresh remotely
     [self refreshUser];
+}
 
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    if ([self shouldPromptToCompleteProfile]) {
+        [self showCompleteProfileView];
+    }
+}
+
+- (BOOL)shouldPromptToCompleteProfile
+{
+    BOOL shouldPromptToCompleteProfile = !self.hasShownCompleteProfileView;
+    if (shouldPromptToCompleteProfile) {
+        User *user = [AppDelegate sharedDelegate].store.currentAccount.currentUser;
+        BOOL profileIncomplete = !user.employeeInfo.jobTitle || !user.manager || !user.department || !user.employeeInfo.birthDate || !user.employeeInfo.officePhone || !user.primaryOfficeLocation;
+        shouldPromptToCompleteProfile = shouldPromptToCompleteProfile && profileIncomplete;
+    }
+    return shouldPromptToCompleteProfile;
+}
+
+- (void)showCompleteProfileView
+{
+    self.hasShownCompleteProfileView = YES;
+    self.completeProfileView.transform = CGAffineTransformMakeTranslation(0, -self.completeProfileView.height);
+    [self.view addSubview:self.completeProfileView];
+    [UIView animateWithDuration:0.3 animations:^{
+        self.completeProfileView.transform = CGAffineTransformIdentity;
+    }];
+}
+
+- (void)dismissCompleteProfileView
+{
+    [UIView animateWithDuration:0.3 animations:^{
+        self.completeProfileView.transform = CGAffineTransformMakeTranslation(0, -self.completeProfileView.height);
+    } completion:^(BOOL finished) {
+        [self.completeProfileView removeFromSuperview];
+    }];
 }
 
 -(void)loadViewFromData {
@@ -117,9 +174,20 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)completeProfileTapped:(id)sender
+{
+    [self showEditAccount];
+}
+
 - (void)avatarTouched:(id)sender
 {
     [[FileLogManager sharedManager] composeEmailWithDebugAttachment];
+}
+
+- (void)showEditAccount
+{
+    EditAccountViewController *editAccountViewController = [[EditAccountViewController alloc] init];
+    [self.navigationController pushViewController:editAccountViewController animated:YES];
 }
 
 -(void)showSettings
