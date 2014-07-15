9bde7113eddc3e6300e599277a03eadad5f65a90
Jeff Ames <jeffreymames@gmail.com>
2014-07-15 10:51:42 -0400

    new sinch credentials

--- Headshot-ios/Constants.m
+++ Headshot-ios/Constants.m
@@ -5,9 +5,9 @@ NSString * const StagingAPIBaseURLString  = @"http://api.badge-staging.com/v1";
 NSString * const SinchProductionAppKey = @"42c9fd75-a981-4d9e-ba79-1c7647df9553";
 NSString * const SinchProductionAppSecret = @"wbnKEXplqUWSDMceOSd4hg==";
 NSString * const SinchProdunctionEnvironmentHost = @"sandbox.sinch.com";
-NSString * const SinchStagingAppKey = @"1c50919d-3597-4b39-ba46-214775a7e677";
-NSString * const SinchStagingAppSecret = @"opKHyqtl8EKkF09dNxalDQ==";
-NSString * const SinchStagingEnvironmentHost = @"clientapi.sinch.com";
+NSString * const SinchStagingAppKey = @"61dd2830-a808-41a5-9b26-8acd1cd4eece";
+NSString * const SinchStagingAppSecret = @"T0arFY+PRUClndBgjLpQlA==";
+NSString * const SinchStagingEnvironmentHost = @"sandbox.sinch.com";
 
 ServerEnvironment CurrentServerEnvironment = ServerEnvironmentStaging;
 
