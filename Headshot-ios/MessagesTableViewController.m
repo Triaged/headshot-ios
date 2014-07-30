9a46a420127f43a2692fdfa98b1173f29146bb47
Jeff Ames <jeffreymames@gmail.com>
2014-07-30 18:59:16 -0400

    smaller message thread previews

--- Headshot-ios/MessagesTableViewController.m
+++ Headshot-ios/MessagesTableViewController.m
@@ -140,7 +140,7 @@
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    return 80.0;
+    return 72.0;
 }
 
 - (UITableViewCell*)tableView:(UITableView*)tableView cellForRowAtIndexPath:(NSIndexPath*)indexPath
